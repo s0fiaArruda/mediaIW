9a5fc3d9b06c641050bb10751348d364bcb0ac53
Sofia Arruda <arruda.sofia2509@gmail.com>
2026-04-08 17:18:58 -0300

    Delete projeto-media directory

--- projeto-media/script.js
+++
@@ -1,8 +0,0 @@
-function calcularMedia() {
-    const nota1= parseFloat(document.getElementById('nota1').value);
-    const nota2= parseFloat(document.getElementById('nota2').value);
-    const media= (nota1+nota2)/2;
-
-    const resultado =document.getElementById('resultado');
-    resultado.innerHTML= `<br> Média: ${media.toFixed(2)}`
-}
